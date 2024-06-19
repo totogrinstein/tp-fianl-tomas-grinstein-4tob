2059abeb9c7e7c0622c5b1c211d53bc03a6675fd
totogrinstein <147621859+totogrinstein@users.noreply.github.com>
2024-06-19 10:37:28 -0300

    tp final

---
+++ Assets/visualizarcript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class visualizarcript : MonoBehaviour
+{
+    public GameObject[] arrayobjetos;
+    private int idx = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            DesactivateALL();
+            arrayobjetos[idx].SetActive(true);
+            if (idx!= arrayobjetos.Length -1)
+            {
+                idx++;
+            }
+
+            Debug.Log(idx);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            DesactivateALL();
+            arrayobjetos[idx].SetActive(true);
+            if (idx != 0)
+            {
+                idx--;
+            }
+            Debug.Log(idx);
+        }
+       
+
+    }
+    void DesactivateALL()
+    {
+        foreach (var objeto in arrayobjetos)
+        {
+            objeto.SetActive(false);
+        }
+    }
+}
